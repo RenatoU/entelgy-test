5f93a3c5e9dadcc3ef3b98ea98a9e7280f198f74
Renato <renatourtecho@gmail.com>
2021-04-20 19:56:45 -0500

    Primer commit

---
+++ src/main/java/com/comments/app/CommentsAppApplication.java
@@ -0,0 +1,13 @@
+package com.comments.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentsAppApplication.class, args);
+	}
+
+}
